609970b7b24df528c496e436f61f148e14c8ab9e
Jardel <jardellima@unigranrio.br>
2018-07-04 05:21:36 -0300

    Corrigindo espaço

--- src/main/java/br/unigranrio/calculadoraIMC/services/Manual.java
+++ src/main/java/br/unigranrio/calculadoraIMC/services/Manual.java
@@ -3,7 +3,7 @@ package br.unigranrio.calculadoraIMC.services;
 public class Manual {
 	public static String manual() {
 		return "<h1>Bem vindo a calculadora de IMC</h1>"
-				+ "As consultas são feitas através do método HTTP<strong>GET</strong><br><br>"
+				+ "As consultas são feitas através do método HTTP <strong>GET</strong><br><br>"
 				+ "Se tiver de 20 anos até 65 anos, utilize /adulto<br>"
 				+ "Se tiver mais de 65 anos, utilize /idoso ou /idosa<br>"
 				+ "Se tiver menos de 20 anos, utilize /menino ou /menina<br>"
